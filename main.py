91436f0f12c6604b487e1097409d977328fa5d78
notencore <en_co_re@yahoo.ca>
2016-01-11 14:24:12 +0000

    Initial switch to Python

---
+++ main.py
@@ -0,0 +1,12 @@
+import re
+
+comparison_string = re.compile(r"^\$(.+):(.+)")
+
+def processString(string):
+    #God I hate four tabs.
+    groups = comparison_string.findall(string)
+    for (key, value) in groups:
+        print(key, value)
+    return
+
+processString("$Hello:World\n$Goodbye:Universe")
