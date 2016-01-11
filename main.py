94b41eb4bd6b54f2b27c6fa88965c6047aa5567b
notencore <en_co_re@yahoo.ca>
2016-01-11 15:33:19 +0000

    Actually fixed conflict

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env/ python
 
 import re
-<<<<<<< HEAD
 import sys
-=======
-import fileinput
->>>>>>> a6dfbd3524e13be8b22cca09fd9f861a26bb266b
 
 var_string = re.compile(r"^\$(.+):(.+)", re.M)
 end_of_vars = re.compile("----")
@@ -16,10 +12,6 @@ def processString(string):
     for (key, value) in groups:
         print(key, value)
     return
-<<<<<<< HEAD
+
 with open(sys.argv[1]) as file:
     processString(file.read())
-=======
-
-processString("$Test:Works")
->>>>>>> a6dfbd3524e13be8b22cca09fd9f861a26bb266b
