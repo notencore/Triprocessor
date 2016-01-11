dc2043514f303642d0390ece28eb3d1bbfe9aa32
notencore <en_co_re@yahoo.ca>
2016-01-11 17:30:43 +0000

    Update Triprocessor, now brute force replaces vars.

--- main.py
+++ main.py
@@ -3,14 +3,16 @@
 import re
 import sys
 
-var_string = re.compile(r"^\$(.+):(.+)", re.M)
+var_string = re.compile(r"^(\$.+):(.+)", re.M)
 end_of_vars = re.compile("----")
 
 def processString(string):
-    #God I hate four tabs.
-    groups = var_string.findall(string, 0, end_of_vars.search(string).start())
+    last_var_pos = end_of_vars.search(string).end()
+    groups = var_string.findall(string, 0, last_var_pos)
+    string = string[last_var_pos:]
     for (key, value) in groups:
-        print(key, value)
+        string = string.replace(key, value)
+    print(string)
     return
 
 with open(sys.argv[1]) as file:
