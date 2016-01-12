0fc8a09ebfaf5adacaf2ddcd504ea297dcd29bf1
notencore <en_co_re@yahoo.ca>
2016-01-12 17:29:04 +0000

    Added text file output

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ from processMarkup import processMarkup
 def processString(original_string):
     working_string = substituteStrings(original_string)
     working_string = processMarkup(working_string)
-    print(working_string)
+    if sys.argv[2]:
+        if raw_input("Overwrite {}? WARNING: Permanent! Y/N: ".format(sys.argv[2])) == "Y":
+            with open(sys.argv[2], "w") as file:
+                file.write(working_string)
+        else:
+            print(working_string)
     return
 
 with open(sys.argv[1]) as file:
